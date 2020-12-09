9921ac23fa794f22110a3c8c0eeb84bacaa4573c
Drantho <drantho@gmail.com>
2020-12-08 21:19:22 -0800

    Moved onload functionality to button, style updates, readme updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 // declare global variables for password criteria and set to user values
-var passwordLength = parseInt(prompt("Enter password length."));
-var lowerCaseCriteria = confirm("Use lower case digits.");
-var upperCaseCriteria = confirm("Use upper case digits");
-var numberCriteria = confirm("Use numeral digits.");
-var specialCriteria = confirm("Use special character digits.");
+// get criteria from user
+var passwordLength;
+var lowerCaseCriteria;
+var upperCaseCriteria;
+var numberCriteria;
+var specialCriteria;
 
 // declare global array varialbles for possible digits
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -11,50 +12,80 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var specialCharacters = ["!", "#", "$", "%", "&", "+", "@"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+function setCriteria() {
+    // get criteria from user
+    
+    passwordLength = parseInt(prompt("Enter password length."));
+    lowerCaseCriteria = confirm("Use lower case digits.");
+    upperCaseCriteria = confirm("Use upper case digits");
+    numberCriteria = confirm("Use numeral digits.");
+    specialCriteria = confirm("Use special character digits.");        
+
+    // display criteria
+    document.getElementById("passwordLength").innerHTML = passwordLength;
+    document.getElementById("useLowerCase").innerHTML = lowerCaseCriteria;
+    document.getElementById("useUpperCase").innerHTML = upperCaseCriteria;
+    document.getElementById("useNumerals").innerHTML = numberCriteria;
+    document.getElementById("useSpecialCharacters").innerHTML = specialCriteria;
+
+    // if invalid criteria set, call this function to try again
+    if(!checkCriteria() || !checkLength()){
+        console.log("attempt to call setCriteria again.");
+        setCriteria();
+    }else{
+        // auto generate a password after criteria is set
+        generatePassword();
+    }    
+}
+
 // main function loops through getDigit() passwordLength times
 function generatePassword() {
-    var password = "";
 
-    // Check to make sure user has selected at least 1 criteria and appropriate length 
-    if (checkCriteria() && checkLength()) {
-        console.log("passwordLength: ", passwordLength)
+    if (passwordLength !== undefined) {
 
-        // loop through and concat getDigit to password passwordLength times
-        for (var i = 0; i < passwordLength; i++) {
-            var tempDigit = getDigit();
-            console.log("tempDigit: ", tempDigit);
-            password = password.concat(tempDigit);
+        var password = "";
+
+        // Check to make sure user has selected at least 1 criteria and appropriate length 
+        if (checkCriteria() && checkLength()) {
+            // console.log("passwordLength: ", passwordLength)
+
+            // loop through and concat getDigit to password passwordLength times
+            for (var i = 0; i < passwordLength; i++) {
+                var tempDigit = getDigit();
+                // console.log("tempDigit: ", tempDigit);
+                password = password.concat(tempDigit);
+            }
+            console.log("Password: ", password);
+
+            // set html - passwordResult element's inner html to password
+            document.getElementById("passwordResult").innerHTML = password;
         }
-        console.log("Password: ", password);
 
-        // set html - passwordResult element's inner html to password
-        document.getElementById("passwordResult").innerHTML = password;
+        // criteria failed. attempt to set valid criteria again
+        else{
+            setCriteria();
+        }
+
     }
-
-    // give user feedback on password requirements
-    // 0 criteria and inappropriate length detected
-    else if (!checkCriteria() && !checkLength()) {
-        alert("Please select at least one criteria and a password length between 8 and 128 inclusive.");
-    }
-
-    // only 0 criteria aspect violated
-    else if (!checkCriteria()) {
-        alert("Please select at least one criteria.")
-    }
-
-    // only inappropriate length entered
+    // page is in initial state. set criteria before generating a password
     else {
-        alert("Please select a password length between 8 and 128 inclusive.");
+        setCriteria();
     }
 }
 
 // check if at least 1 criteria selected will return true if at least 1 criteria selected - false otherwise
 function checkCriteria() {
+    if(!lowerCaseCriteria && !upperCaseCriteria && !numberCriteria && !specialCriteria){
+        alert("Select at least one criteria");
+    }
     return lowerCaseCriteria || upperCaseCriteria || numberCriteria || specialCriteria;
 }
 
 // check if appropriate length entered - true if between 8 and 128 inclusive
 function checkLength() {
+    if(passwordLength < 8 || passwordLength > 128){
+        alert("Password must be at least 8 characters and no more than 128 characters");
+    }
     return passwordLength >= 8 && passwordLength <= 128;
 }
 
@@ -66,7 +97,7 @@ function getDigit() {
 
     // adds all lower case digits to possibleDigits if lowerCaseCriteria === true 
     if (lowerCaseCriteria) {
-        console.log("lowerCaseCriteria: " + lowerCaseCriteria + " adding", lowerCase);
+        // console.log("lowerCaseCriteria: " + lowerCaseCriteria + " adding", lowerCase);
         possibleDigits = possibleDigits.concat(lowerCase);
     }
 
@@ -85,20 +116,16 @@ function getDigit() {
         possibleDigits = possibleDigits.concat(specialCharacters);
     }
 
-    console.log("possibleDigits: ", possibleDigits);
+    // console.log("possibleDigits: ", possibleDigits);
 
     // select random element from possible digits and return it
     return possibleDigits[Math.floor(Math.random() * possibleDigits.length)];
 }
 
-function reloadPage(){
-    location.reload();
-}
-
-function showCriteria(){
-    document.getElementById("passwordLength").innerHTML = passwordLength;
-    document.getElementById("useLowerCase").innerHTML = lowerCaseCriteria;
-    document.getElementById("useUpperCase").innerHTML = upperCaseCriteria;
-    document.getElementById("useNumerals").innerHTML = numberCriteria;
-    document.getElementById("useSpecialCharacters").innerHTML = specialCriteria;
+// copy password to clipboard
+function copy() {
+    var copyText = document.getElementById("passwordResult");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
 }
